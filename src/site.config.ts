a62d2c46290dca8efb94bc3ae4ab9b424b850568
lsy <lsystudyhard@163.com>
2026-05-19 20:30:50 +0800

    增加life页面

--- src/site.config.ts
+++ src/site.config.ts
@@ -52,6 +52,7 @@ export const theme: ThemeUserConfig = {
       { title: 'Docs', link: '/docs' },
       { title: 'Projects', link: '/projects' },
       { title: 'Links', link: '/links' },
+      { title: 'Links', link: '/life' },
       { title: 'About', link: '/about' }
     ]
   },
